6a1635580223e4a8154e7ab02acbb9a3c4373c4c
Peter <peter.september@takealot.com>
2020-01-23 08:57:52 +0200

    writing to csv instead of txt

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 print(__name__)
 
@@ -18,11 +19,19 @@ def write_to_file(data):
 		message = data["message"]
 		file = database.write(f'\n{email},{subject},{message}')
 
+def write_to_csv(data):
+	with open('database.csv',newline='', mode='a') as database2:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		csv_writer = csv.writer(database2, delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
 	if request.method =='POST':
 		data =request.form.to_dict()
-		write_to_file(data)
+		write_to_csv(data)
 		return redirect('/thankyou.html')
 	else:
 		return 'something went wrong, try again'
